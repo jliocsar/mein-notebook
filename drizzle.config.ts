9714b52c91819d896ab62ee6ba32ac8628508662
jliocsar <juliocesarmfo@gmail.com>
2023-10-14 16:07:26 -0300

    fix config write path

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,17 +1,24 @@
+import { stderr, exit } from 'node:process'
 import * as fs from 'node:fs'
 import * as path from 'node:path'
 
 import type { Config } from 'drizzle-kit'
+import * as C from 'colorette'
 
 export type TDbCredentials = {
   url: string
   authToken: string
 }
 
-const dbCredentialsPath = path.resolve(
-  import.meta.dir,
-  '.meine-notebook/database',
-)
+const dbCredentialsPath = path.resolve(import.meta.dir, '.notiz/database')
+if (!fs.existsSync(dbCredentialsPath)) {
+  stderr.write(
+    `Database credentials not found at ${dbCredentialsPath}. Please create them by running ${C.cyanBright(
+      'notiz auth',
+    )}.`,
+  )
+  exit(1)
+}
 const dbCredentials = fs.readFileSync(dbCredentialsPath)
 
 export default {
